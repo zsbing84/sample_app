c72688b1c2be6b5769584827c0f983cac54dc259
U-SHUU-9E12232EC4\shuu <shuu@shuu-9e12232ec4.(none)>
2010-10-09 18:40:24 +0900

    rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'd18876edc6262b31a785be18f1fca4be0a1147973bc38c7c26b01358224ec3a9915ed0a5450c479e950e98cb464c2e8d3e1ad9dfc0c4c2451c55451543852bfc'
