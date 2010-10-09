c1e513ae76d6e6255fbc261b971bd32a9ae2c2d8
zsbing84 <zsbing84@hotmail.com>
2010-10-10 01:42:31 +0900

    test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe UsersController do
+	render_views
 
   describe "GET 'new'" do
     it "should be successful" do
@@ -10,7 +11,7 @@ describe UsersController do
     
     it "should have the right title" do
       get 'new'
-      response.should have_selector("title", :content => "Sign up")
+      response.should have_selector("title", :content => "| Sign up")
     end
 
   end
