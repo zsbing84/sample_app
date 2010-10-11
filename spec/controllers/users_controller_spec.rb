acfd9caa8e3b10c547a0cebf8821bf1be293f2c0
zsbing84 <zsbing84@hotmail.com>
2010-10-11 11:47:36 +0900

    User model with password

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -13,7 +13,22 @@ describe UsersController do
       get 'new'
       response.should have_selector("title", :content => "| Sign up")
     end
+  end
+  
+	describe "GET 'show'" do
+    before(:each) do
+      @user = Factory(:user)
+    end
 
+    it "should be successful" do
+      get :show, :id => @user
+      response.should be_success
+    end
+
+    it "should find the right user" do
+      get :show, :id => @user
+      assigns(:user).should == @user
+    end
   end
   
 end
