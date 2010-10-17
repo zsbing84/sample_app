95cfa9819f0de8bdedf95c6d4422bee81d2180c0
zsbing84 <zsbing84@hotmail.com>
2010-10-17 23:40:35 +0900

    add activation-2

--- db/migrate/20101012145413_add_persistence_token_to_users.rb
+++ db/migrate/20101012145413_add_persistence_token_to_users.rb
@@ -1,6 +1,6 @@
 class AddPersistenceTokenToUsers < ActiveRecord::Migration
   def self.up
-    add_column :users, :persistence_token, :string
+    add_column :users, :persistence_token, :string, :default => false, :null => false
   end
 
   def self.down
